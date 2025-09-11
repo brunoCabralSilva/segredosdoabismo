60d4caaf6d4281d5a42cec5a1b2fc6c077447c8f
Bruno <bruno.cabral.silva2018@gmail.com>
2025-09-11 00:03:26 -0300

    imagens para o site

--- src/app/doctrines/page.tsx
+++ src/app/doctrines/page.tsx
@@ -1,23 +1,82 @@
 "use client"
-
-import Footer from "@/components/footer"
-import Nav from "@/components/nav"
+import Footer from "@/components/footer";
+import Nav from "@/components/nav";
+import listDoctrines from '../../data/doctrines.json';
+import listHouses from '../../data/houses.json';  // Aqui você importa ou define o seu arquivo de casas
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Doctrines() {
-  return(
+  // Agrupar as doutrinas pelo campo 'belonging'
+  const groupedDoctrines = listDoctrines.reduce((acc, doctrine) => {
+    const group = doctrine.belonging;
+    if (!acc[group]) {
+      acc[group] = [];
+    }
+    acc[group].push(doctrine);
+    return acc;
+  }, {});
+
+  // Função para pegar o 'namePtBr' correspondente ao 'belonging' a partir de listHouses
+  const getHouseName = (belonging: string) => {
+    const house = listHouses.find(house => house.name === belonging);
+    return house ? house.namePtBr : 'Comuns';  // Caso não encontre, retorna o próprio belonging
+  };
+
+  return (
     <div className="w-full bg-[] bg-cover bg-top relative">
-    <Nav />
-    <div className="absolute w-full h-full bg-black/80" />
-    <section className="mb-2 min-h-screen relative pb-5">
-    <div className="py-6 px-5 bg-black/90 text-white flex flex-col items-center sm:items-start text-justify">
-        <h1 className="text-4xl relative">Doutrinas</h1>
-        <hr className="w-10/12 my-6" />
-        <p className="pb-2">
-        
-        </p>
+      <Nav />
+      <div className="absolute w-full h-full bg-black/80" />
+      <section className="mb-2 min-h-screen relative pb-5">
+        <div className="py-6 px-5 bg-black/90 text-white flex flex-col items-center sm:items-start text-justify">
+          <h1 className="text-4xl relative">Doutrinas</h1>
+          <hr className="w-10/12 my-6" />
+          <p className="pb-2"></p>
+
+          <div className="w-full relative text-white px-4 pb-4">
+            {
+              Object.keys(groupedDoctrines).map((group, index) => (
+                <div key={index} className="mb-6">
+                  <h2 className="text-2xl font-semibold text-center mb-4">
+                    { getHouseName(group) }
+                  </h2>
+                  <div className="grid grid-cols-1 mobile:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3">
+                    {
+                      groupedDoctrines[group].map((doctrine, index) => (
+                        <Link
+                          key={index}
+                          href={`/doctrines/${doctrine.id}`}
+                          className="border-white border-2 p-3 flex items-center justify-center flex-col bg-trybes-background bg-center relative cursor-pointer h-20vh"
+                        >
+                          <div className="absolute w-full h-full bg-black/80" />
+                          <Image
+                            src={`/images/glifs/${doctrine.belonging}.png`}
+                            alt={`Glifo ${doctrine.name}`}
+                            className="h-14 w-20 object-contain object-center mb-3 z-20"
+                            width={1200}
+                            height={800}
+                          />
+                          <p className="relative font-bold text-center z-20">
+                            {doctrine.name}
+                          </p>
+                        </Link>
+                      ))
+                    }
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+          <button
+            type="button"
+            className="pb-3 px-6 text-orange-300 hover:text-orange-600 transition-colors duration-300 mt-5 cursor-pointer underline"
+          >
+            Enviar Feedback
+          </button>
+        </div>
+      </section>
+      <Footer />
     </div>
-    </section>
-    <Footer />
-</div>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
 
   return (
     <main className="bg-black flex flex-col items-center justify-center relative">
-      <header className="h-screen w-full flex flex-col items-center justify-center bg-cover relative bg-[url('/images/wallpapers/01.jpg')] bg-top">
+      <header className="h-screen w-full flex flex-col items-center justify-center bg-cover relative bg-[url('/images/wallpapers/06.png')] bg-top">
         <div className={`absolute w-full h-full bg-black/50`} />
         <div className="h-screen w-full z-10 flex flex-col justify-center items-center">
-          {/* <Image
-            src="/images/logos/text.png"
+          <Image
+            src="/images/logos/text3.png"
             alt="Nome 'Werewolf the Apocalypse' em formato de imagem"
             className="h-[50vh] sm:h-[70vh] md:h-[50vh] w-10/12 sm:w-3/5 md:w-1/2 xl:w-5/12 object-contain"
             width={2000}
             height={800}
             priority
-          /> */}
+          />
           <Image
             src="/images/logos/arrow-down.png"
             alt="seta para baixo"
@@ -37,50 +37,50 @@ export default function Home() {
       </header>
       <div ref={ describe } className="grid grid-cols-2 sm:grid-cols-8 w-full h-full px-4 sm:px-8 pb-4 gap-4 sm:gap-6">
         <div
-          className={`sm:text-3xl bg-[url('/images/wallpapers/01.jpg')] mt-4 sm:mt-8 bg-cover col-span-2 sm:col-span-8 sm:row-span-1 text-center text-white flex relative cursor-pointer border-transparent items-end`}>
+          className={`sm:text-3xl bg-[url('/images/wallpapers/05.png')] mt-4 sm:mt-8 bg-cover bg-top col-span-2 sm:col-span-8 sm:row-span-1 text-center text-white flex relative border-transparent items-end`}>
           <div className={`absolute w-full h-full bg-black/40`} />
           <p className="py-14 p-5 z-10 relative">
-            Seja bem-vindo a uma jornada marcada por ecos da Criação e do Abismo, onde você poderá desvendar os segredos dos Anjos Caídos, seus Pactos, Facções e Tormentos em Demônio: a Queda.
+            {/* Seja bem-vindo a uma jornada marcada por ecos da Criação e do Abismo, onde você poderá desvendar os segredos dos Anjos Caídos, seus Pactos, Facções e Tormentos em Demônio: a Queda. */}
           </p>
         </div>
         <Link href="/factions" className="col-span-1 sm:col-span-2 sm:row-span-2">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover h-full sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/02.png')] bg-cover bg-center h-full sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2 relative">Facções</p>
           </div>
         </Link>
         <Link href="/doctrines" className="col-span-1 sm:col-span-2 sm:row-span-4">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover h-[20vh] sm:h-full text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/03.png')] bg-center bg-cover h-[20vh] sm:h-full text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2">Doutrinas</p>
           </div>
         </Link>
         <Link href="/sessions" className="col-span-2 sm:col-span-4 sm:row-span-2">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover bg-center h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/07.png')] bg-cover bg-top h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2">Sessões</p>
           </div>
         </Link>
         <Link href="/houses" className="col-span-1 sm:col-span-2 sm:row-span-2">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/04.png')] bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2 relative">Casas</p>
           </div>
         </Link>
         <Link href="/backgrounds" className="col-span-1 row-span-2 sm:col-span-2 sm:row-span-2">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover h-full sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/05.png')] bg-cover bg-bottom h-full sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2">Antecedentes</p>
           </div>
         </Link>
         <Link href="/profile" className="col-span-1 sm:row-span-2 sm:col-span-2">
           <div
-            className={`bg-[url('/images/wallpapers/01.jpg')] bg-cover h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
+            className={`bg-[url('/images/wallpapers/08.png')] bg-cover bg-bottom h-[20vh] sm:h-[40vh] text-white flex relative cursor-pointer border-transparent items-end`}>
             <div className={`absolute w-full h-full bg-black/40`} />
             <p className="z-10 font-bold text-base sm:text-xl px-3 p-2">Perfil</p>
           </div>
